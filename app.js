6fbee334dfca9301a1275ca8ef0712a6f2805258
prayagpc <prayagpc28@gmail.com>
2023-10-22 20:08:45 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+const app= express();
+const port= 3000;
+
+app.use(express.static("public"));
+
+app.get("/",async(req,res)=>{
+    try{
+        const response= await axios.get("https://api.adviceslip.com/advice");
+        const data= response.data;
+
+        res.render("index.ejs", {
+            data: data,
+        });
+    }
+    catch(error){
+        console.log(error.message);
+        res.render("index.ejs", {
+            error : "Can't load the advice",
+        });
+    }
+})
+app.listen(port,()=>{
+    console.log(`Server running on port ${port}`);
+})
